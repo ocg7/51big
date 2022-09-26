41484018b9b0f381b11f2bbea4a244a3319cfcc6
wangkang <18605119264@163.com>
2022-09-26 18:41:53 +0800

    2022/09/26做了个开头

---
+++ assets/js/login.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('#go2Reg').on('click',function(){
+    $('.login-wrap').hide()
+    $('.reg-wrap').show()
+  })
+  $('#go2Login').on('click',function(){
+    $('.login-wrap').show()
+    $('.reg-wrap').hide()
+  })
+})
